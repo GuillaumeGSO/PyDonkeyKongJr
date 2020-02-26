0782dbc93807f460a4fbeb23221073cfe3eac48f
GuillaumeGSO <GuillaumeGSO@users.noreply.github.com>
2020-02-26 22:31:29 +0100

    add sound and blink when missing

--- Missed.py
+++ Missed.py
@@ -19,10 +19,17 @@ class Missed():
         self.allPositions = self.generate()
         self.spritePosition = [self.allPositions.get("M00"), 
         self.allPositions.get("M01"), self.allPositions.get("M02")]
+        self.sound = makeSound("sounds/Missed.wav")
         
 
     def update(self, num_error):
         spriteToAdd = self.spritePosition[num_error]
         moveSprite(spriteToAdd.sprite,
                    spriteToAdd.x, spriteToAdd.y)
+        playSound(self.sound)
+        for i in range(5):
+            showSprite(spriteToAdd.sprite)
+            pause(150)
+            hideSprite(spriteToAdd.sprite)
+            pause(150)
         showSprite(spriteToAdd.sprite)
